7ccc56d5e0c26c1a99ff52d02d0f085acfcffa37
Dan Gowans <dangowans@users.noreply.github.com>
2025-03-25 12:22:21 -0400

    linting

--- database/getReportData.d.ts
+++ database/getReportData.d.ts
@@ -1,2 +1,2 @@
-export type ReportParameters = Record<string, string | number>;
-export default function getReportData(reportName: string, reportParameters?: ReportParameters): Promise<unknown[] | undefined>;
+export type ReportParameters = Record<string, number | string>;
+export default function getReportData(reportName: string, reportParameters?: ReportParameters): Promise<undefined | unknown[]>;
