89ade3ba1c2efd33cfe58aa858e0e9d8b234c6cb
Dan Gowans <dangowans@users.noreply.github.com>
2025-08-15 13:16:58 -0400

    remove unused script

--- public/javascripts/dashboard.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-(() => {
-    const sunrise = exports.sunrise;
-    const workOrderNumberCircleElements = document.querySelectorAll('.fa-circle[data-work-order-number]');
-    for (const workOrderNumberCircleElement of workOrderNumberCircleElements) {
-        workOrderNumberCircleElement.style.color = sunrise.getRandomColor(workOrderNumberCircleElement.dataset.workOrderNumber ?? '');
-    }
-})();
