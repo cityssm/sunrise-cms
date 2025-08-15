6cc2f5c4479d386e7f2ff38b35dd178f6cbb3c84
Dan Gowans <dangowans@users.noreply.github.com>
2025-08-15 13:14:13 -0400

    remove milestones from dashboard

--- handlers/dashboard-get/dashboard.js
+++ handlers/dashboard-get/dashboard.js
@@ -1,16 +1,5 @@
-import { dateToString } from '@cityssm/utils-datetime';
-import getWorkOrderMilestones from '../../database/getWorkOrderMilestones.js';
-export default async function handler(_request, response) {
-    const currentDateString = dateToString(new Date());
-    const workOrderMilestones = await getWorkOrderMilestones({
-        workOrderMilestoneDateFilter: 'date',
-        workOrderMilestoneDateString: currentDateString
-    }, {
-        includeWorkOrders: true,
-        orderBy: 'completion'
-    });
+export default function handler(_request, response) {
     response.render('dashboard', {
-        headTitle: 'Dashboard',
-        workOrderMilestones
+        headTitle: 'Dashboard'
     });
 }
--- handlers/dashboard-get/dashboard.ts
+++ handlers/dashboard-get/dashboard.ts
@@ -1,27 +1,7 @@
-import { dateToString } from '@cityssm/utils-datetime'
 import type { Request, Response } from 'express'
 
-import getWorkOrderMilestones from '../../database/getWorkOrderMilestones.js'
-
-export default async function handler(
-  _request: Request,
-  response: Response
-): Promise<void> {
-  const currentDateString = dateToString(new Date())
-
-  const workOrderMilestones = await getWorkOrderMilestones(
-    {
-      workOrderMilestoneDateFilter: 'date',
-      workOrderMilestoneDateString: currentDateString
-    },
-    {
-      includeWorkOrders: true,
-      orderBy: 'completion'
-    }
-  )
-
+export default function handler(_request: Request, response: Response): void {
   response.render('dashboard', {
-    headTitle: 'Dashboard',
-    workOrderMilestones
+    headTitle: 'Dashboard'
   })
 }
