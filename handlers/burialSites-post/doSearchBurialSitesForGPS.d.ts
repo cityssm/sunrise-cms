8ff0e2bbe69cbee30f57c04ea588632474278c15
Dan Gowans <dangowans@users.noreply.github.com>
2025-08-26 11:26:36 -0400

    temporarily remove to fix file name case

--- handlers/burialSites-post/doSearchBurialSitesForGPS.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response } from 'express';
-import { type GetBurialSitesFilters } from '../../database/getBurialSites.js';
-export default function handler(request: Request<unknown, unknown, GetBurialSitesFilters>, response: Response): void;
