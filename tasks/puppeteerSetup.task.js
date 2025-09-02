d654c69bebc524c96370b71fef2d54605268d9bd
Dan Gowans <dangowans@users.noreply.github.com>
2025-09-02 10:48:53 -0400

    option to run puppeteerSetup task
    no longer a postinstall script

--- tasks/puppeteerSetup.task.js
+++ tasks/puppeteerSetup.task.js
@@ -1,8 +1,12 @@
 import { testInstalledBrowser } from '@cityssm/pdf-puppeteer';
 import Debug from 'debug';
-import { DEBUG_NAMESPACE } from '../debug.config.js';
+import { DEBUG_ENABLE_NAMESPACES, DEBUG_NAMESPACE } from '../debug.config.js';
 import { getConfigProperty } from '../helpers/config.helpers.js';
+if (process.env.NODE_ENV === 'development') {
+    Debug.enable(DEBUG_ENABLE_NAMESPACES);
+}
 const debug = Debug(`${DEBUG_NAMESPACE}:puppeteerSetup`);
 const browser = getConfigProperty('settings.printPdf.browser');
 debug('Testing browser: %o', browser);
-await testInstalledBrowser(browser, true);
+const result = await testInstalledBrowser(browser, true);
+debug('Browser test result: %o', result);
