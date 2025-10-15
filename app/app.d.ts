1648afdf42aa84fd0eaf621c10b314d526f28553
Dan Gowans <dangowans@users.noreply.github.com>
2025-10-15 11:09:37 -0400

    fix type issues

--- app/app.d.ts
+++ app/app.d.ts
@@ -1,3 +1,3 @@
-export declare const app: any;
+export declare const app: import("express-serve-static-core").Express;
 export default app;
 export { shutdown as shutdownAbuseCheck } from '@cityssm/express-abuse-points';
