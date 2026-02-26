70375fbdda92a9b515a1e98fd884d52157433f79
Dan Gowans <dangowans@users.noreply.github.com>
2026-02-26 15:42:18 -0500

    linting

--- handlers/admin-post/doUpdateIntermentDepth.ts
+++ handlers/admin-post/doUpdateIntermentDepth.ts
@@ -4,13 +4,14 @@ import updateIntermentDepth, {
   type UpdateIntermentDepthForm
 } from '../../database/updateIntermentDepth.js'
 import { getCachedIntermentDepths } from '../../helpers/cache/intermentDepths.cache.js'
-
 import type { IntermentDepth } from '../../types/record.types.js'
 
-
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- Works on client side
-export type DoUpdateIntermentDepthResponse =
-  { success: boolean; intermentDepths: IntermentDepth[] }
+export type DoUpdateIntermentDepthResponse = {
+  success: boolean
+
+  intermentDepths: IntermentDepth[]
+}
 
 export default function handler(
   request: Request<unknown, unknown, UpdateIntermentDepthForm>,
