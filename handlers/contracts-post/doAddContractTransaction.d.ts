94be70469119a32d978aee88b62297095d40dc27
Dan Gowans <dangowans@users.noreply.github.com>
2026-02-27 09:54:10 -0500

    attempts to fix code quality ai

--- handlers/contracts-post/doAddContractTransaction.d.ts
+++ handlers/contracts-post/doAddContractTransaction.d.ts
@@ -2,10 +2,10 @@ import type { Request, Response } from 'express';
 import { type AddTransactionForm } from '../../database/addContractTransaction.js';
 import type { ContractTransaction } from '../../types/record.types.js';
 export type DoAddContractTransactionResponse = {
-    success: true;
-    contractTransactions: ContractTransaction[];
-} | {
     errorMessage: string;
     success: false;
+} | {
+    success: true;
+    contractTransactions: ContractTransaction[];
 };
 export default function handler(request: Request<unknown, unknown, AddTransactionForm>, response: Response<DoAddContractTransactionResponse>): Promise<void>;
