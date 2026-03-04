56b9399c514f2b7c67e5972cce37169d7e480b05
Dan Gowans <dangowans@users.noreply.github.com>
2026-03-04 14:24:56 -0500

    compile typescript

--- database/deleteUser.js
+++ database/deleteUser.js
@@ -1,7 +1,14 @@
 import sqlite from 'better-sqlite3';
+import { getConfigProperty } from '../helpers/config.helpers.js';
 import { sunriseDB } from '../helpers/database.helpers.js';
+import createAuditLogEntries from './createAuditLogEntries.js';
+import getUser from './getUser.js';
+const auditLogIsEnabled = getConfigProperty('settings.auditLog.enabled');
 export function deleteLocalUser(userName, user, connectedDatabase) {
     const database = connectedDatabase ?? sqlite(sunriseDB);
+    const recordBefore = auditLogIsEnabled
+        ? getUser(userName, database)
+        : undefined;
     const rightNowMillis = Date.now();
     const result = database
         .prepare(/* sql */ `
@@ -14,6 +21,20 @@ export function deleteLocalUser(userName, user, connectedDatabase) {
         AND recordDelete_timeMillis IS NULL
     `)
         .run(user.userName, rightNowMillis, userName);
+    if (result.changes > 0 && auditLogIsEnabled) {
+        createAuditLogEntries({
+            mainRecordType: 'user',
+            mainRecordId: userName,
+            updateTable: 'Users'
+        }, [
+            {
+                property: '*',
+                type: 'deleted',
+                from: recordBefore,
+                to: undefined
+            }
+        ], user, database);
+    }
     if (connectedDatabase === undefined) {
         database.close();
     }
