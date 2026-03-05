ca39767118828b4a7f132378ad2626a767fdc576
Dan Gowans <dangowans@users.noreply.github.com>
2026-03-05 15:38:53 -0500

    do compiles

---
+++ cypress/e2e/03-readOnly/funeralHomes.cy.js
@@ -0,0 +1,32 @@
+import { testView } from '../../../test/_globals.js';
+import { ajaxDelayMillis, checkA11yLog, login, logout } from '../../support/index.js';
+describe('Funeral Home Search', () => {
+    beforeEach(() => {
+        logout();
+        login(testView);
+    });
+    afterEach(logout);
+    it('Has no detectable accessibility issues on the search page', () => {
+        cy.visit('/funeralHomes');
+        cy.location('pathname').should('equal', '/funeralHomes');
+        cy.wait(ajaxDelayMillis);
+        cy.injectAxe();
+        cy.checkA11y(undefined, undefined, checkA11yLog);
+    });
+    it('Can view a funeral home from the search results', () => {
+        cy.visit('/funeralHomes');
+        cy.location('pathname').should('equal', '/funeralHomes');
+        cy.wait(ajaxDelayMillis);
+        cy.get('#container--searchResults a.has-text-weight-bold')
+            .first()
+            .then(($link) => {
+            const href = $link.attr('href');
+            expect(href).to.include('/funeralHomes/');
+            cy.wrap($link).click();
+            cy.location('pathname').should('include', '/funeralHomes/');
+            cy.log('Check accessibility on the funeral home view page');
+            cy.injectAxe();
+            cy.checkA11y(undefined, undefined, checkA11yLog);
+        });
+    });
+});
---
+++ cypress/e2e/03-readOnly/workOrders.cy.js
@@ -0,0 +1,32 @@
+import { testView } from '../../../test/_globals.js';
+import { ajaxDelayMillis, checkA11yLog, login, logout } from '../../support/index.js';
+describe('Work Order Search', () => {
+    beforeEach(() => {
+        logout();
+        login(testView);
+    });
+    afterEach(logout);
+    it('Has no detectable accessibility issues on the search page', () => {
+        cy.visit('/workOrders');
+        cy.location('pathname').should('equal', '/workOrders');
+        cy.wait(ajaxDelayMillis);
+        cy.injectAxe();
+        cy.checkA11y(undefined, undefined, checkA11yLog);
+    });
+    it('Can view a work order from the search results', () => {
+        cy.visit('/workOrders');
+        cy.location('pathname').should('equal', '/workOrders');
+        cy.wait(ajaxDelayMillis);
+        cy.get('#container--searchResults a.has-text-weight-bold')
+            .first()
+            .then(($link) => {
+            const href = $link.attr('href');
+            expect(href).to.include('/workOrders/');
+            cy.wrap($link).click();
+            cy.location('pathname').should('include', '/workOrders/');
+            cy.log('Check accessibility on the work order view page');
+            cy.injectAxe();
+            cy.checkA11y(undefined, undefined, checkA11yLog);
+        });
+    });
+});
