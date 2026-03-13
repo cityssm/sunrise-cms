b7109a93f6a2c4aea90415560275eb3c699292ef
Dan Gowans <dangowans@users.noreply.github.com>
2026-03-13 10:51:22 -0400

    avoid overtesting for dead links

--- cypress/support/deadLinks.js
+++ cypress/support/deadLinks.js
@@ -1,9 +1,17 @@
-const testedLinks = new Set();
+// Initialize with links to ignore because they are on almost every page
+const testedLinks = new Set([
+    'https://cityssm.github.io/sunrise-cms',
+    'https://cityssm.github.io/sunrise-cms/',
+    'https://cityssm.github.io/sunrise-cms/docs',
+    'https://cityssm.github.io/sunrise-cms/docs/',
+    'https://github.com/cityssm/sunrise-cms',
+    'https://github.com/cityssm/sunrise-cms/releases'
+]);
 export function checkDeadLinks() {
     cy.get('a[href^="https://"]').each(($link) => {
         const href = $link.attr('href');
         // Check if this link has already been tested
-        if (testedLinks.has(href)) {
+        if (testedLinks.has(href) || testedLinks.has(`${href}/`)) {
             cy.log(`Skipping already tested link: ${href}`);
             return;
         }
--- cypress/support/deadLinks.ts
+++ cypress/support/deadLinks.ts
@@ -1,11 +1,19 @@
-const testedLinks = new Set<string>()
+// Initialize with links to ignore because they are on almost every page
+const testedLinks = new Set<string>([
+  'https://cityssm.github.io/sunrise-cms',
+  'https://cityssm.github.io/sunrise-cms/',
+  'https://cityssm.github.io/sunrise-cms/docs',
+  'https://cityssm.github.io/sunrise-cms/docs/',
+  'https://github.com/cityssm/sunrise-cms',
+  'https://github.com/cityssm/sunrise-cms/releases'
+])
 
 export function checkDeadLinks(): void {
   cy.get('a[href^="https://"]').each(($link) => {
     const href = $link.attr('href') as string
 
     // Check if this link has already been tested
-    if (testedLinks.has(href)) {
+    if (testedLinks.has(href) || testedLinks.has(`${href}/`)) {
       cy.log(`Skipping already tested link: ${href}`)
       return
     }
