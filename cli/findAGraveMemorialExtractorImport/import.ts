293551650b122b081ef865157d318b23896c00f4
Dan Gowans <dangowans@users.noreply.github.com>
2026-03-30 11:20:15 -0400

    archive progress

---
+++ cli/findAGraveMemorialExtractorImport/import.ts
@@ -0,0 +1,36 @@
+import Debug from 'debug'
+
+import getCemetery from '../../database/getCemetery.js'
+import { DEBUG_NAMESPACE } from '../../debug.config.js'
+import { sunriseDB, useTestDatabases } from '../../helpers/database.helpers.js'
+
+const debug = Debug(
+  `${DEBUG_NAMESPACE}:findAGraveMemorialExtractorImport:import`
+)
+
+export default async function runFindAGraveMemorialExtractorImport(
+  cemeteryId: string,
+  pathToCsvFile: string
+): Promise<void> {
+  /*
+   * Ensure the script is being run against the testing database.
+   */
+
+  debug('Database: ', sunriseDB)
+
+  if (!useTestDatabases) {
+    throw new Error(
+      'This script should only be run against the testing database.'
+    )
+  }
+
+  /*
+   * Ensure the specified cemetery exists in the database.
+   */
+
+  const cemetery = getCemetery(cemeteryId)
+
+  if (cemetery === undefined) {
+    throw new Error(`Cemetery with ID ${cemeteryId} not found.`)
+  }
+}
