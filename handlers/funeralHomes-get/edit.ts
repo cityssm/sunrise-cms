a69c3b01a22be92068adf5e30605d1d3d8881427
Dan Gowans <dangowans@users.noreply.github.com>
2026-04-02 10:59:51 -0400

    show upcoming services on edit

--- handlers/funeralHomes-get/edit.ts
+++ handlers/funeralHomes-get/edit.ts
@@ -1,25 +1,66 @@
+import sqlite from 'better-sqlite3'
+import Debug from 'debug'
 import type { Request, Response } from 'express'
 
+import getContracts from '../../database/getContracts.js'
 import getFuneralHome from '../../database/getFuneralHome.js'
+import { DEBUG_NAMESPACE } from '../../debug.config.js'
 import { getConfigProperty } from '../../helpers/config.helpers.js'
+import { sunriseDB } from '../../helpers/database.helpers.js'
 
-export default function handler(
+const debug = Debug(`${DEBUG_NAMESPACE}:handlers:funeralHomes:edit`)
+
+export default async function handler(
   request: Request<{ funeralHomeId: string }>,
   response: Response
-): void {
-  const funeralHome = getFuneralHome(request.params.funeralHomeId)
+): Promise<void> {
+  let database: sqlite.Database | undefined
 
-  if (funeralHome === undefined) {
-    response.redirect(
-      `${getConfigProperty('reverseProxy.urlPrefix')}/funeralHomes/?error=funeralHomeIdNotFound`
+  try {
+    database = sqlite(sunriseDB)
+
+    const funeralHome = getFuneralHome(request.params.funeralHomeId)
+
+    if (funeralHome === undefined) {
+      response.redirect(
+        `${getConfigProperty('reverseProxy.urlPrefix')}/funeralHomes/?error=funeralHomeIdNotFound`
+      )
+      return
+    }
+
+    const contracts = await getContracts(
+      {
+        funeralHomeId: funeralHome.funeralHomeId,
+        funeralTime: 'upcoming'
+      },
+      {
+        limit: -1,
+        offset: 0,
+
+        orderBy: 'c.funeralDate, c.funeralTime, c.contractId',
+
+        includeFees: false,
+        includeInterments: true,
+        includeTransactions: false
+      },
+      database
     )
-    return
+
+    response.render('funeralHomes/edit', {
+      headTitle: funeralHome.funeralHomeName,
+
+      funeralHome,
+
+      contracts: contracts.contracts,
+      isCreate: false
+    })
+  } catch (error) {
+    debug(error)
+
+    response
+      .status(500)
+      .json({ errorMessage: 'Database error', success: false })
+  } finally {
+    database?.close()
   }
-
-  response.render('funeralHomes/edit', {
-    headTitle: funeralHome.funeralHomeName,
-
-    funeralHome,
-    isCreate: false
-  })
 }
