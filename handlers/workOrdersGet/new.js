c7d407606d69b41f74dcd521cc11f4981105a70c
Dan Gowans <dangowans@users.noreply.github.com>
2026-08-27 09:50:25 -0400

    set work order dropdowns on create

--- handlers/workOrdersGet/new.js
+++ handlers/workOrdersGet/new.js
@@ -4,12 +4,14 @@ import { getCachedWorkOrderTypes } from '../../helpers/cache/workOrderTypes.cach
 import { i18next } from '../../helpers/i18n.helpers.js';
 export default function handler(request, response) {
     const currentDate = new Date();
+    const workOrderTypes = getCachedWorkOrderTypes();
+    const workOrderStatuses = getCachedWorkOrderStatuses();
     const workOrder = {
+        workOrderStatusId: workOrderStatuses.length > 0 ? workOrderStatuses[0].workOrderStatusId : undefined,
+        workOrderTypeId: workOrderTypes.length > 0 ? workOrderTypes[0].workOrderTypeId : undefined,
         workOrderOpenDate: dateToInteger(currentDate),
         workOrderOpenDateString: dateToString(currentDate)
     };
-    const workOrderTypes = getCachedWorkOrderTypes();
-    const workOrderStatuses = getCachedWorkOrderStatuses();
     response.render('workOrders/edit', {
         headTitle: i18next.t('workOrders.newWorkOrder', {
             lng: response.locals.lng
--- handlers/workOrdersGet/new.ts
+++ handlers/workOrdersGet/new.ts
@@ -9,13 +9,16 @@ import type { WorkOrder } from '../../types/record.types.js'
 export default function handler(request: Request, response: Response): void {
   const currentDate = new Date()
 
+    const workOrderTypes = getCachedWorkOrderTypes()
+    const workOrderStatuses = getCachedWorkOrderStatuses()
+
   const workOrder: Partial<WorkOrder> = {
+    workOrderStatusId: workOrderStatuses.length > 0 ? workOrderStatuses[0].workOrderStatusId : undefined,
+    workOrderTypeId: workOrderTypes.length > 0 ? workOrderTypes[0].workOrderTypeId : undefined,
+
     workOrderOpenDate: dateToInteger(currentDate),
     workOrderOpenDateString: dateToString(currentDate)
   }
-
-  const workOrderTypes = getCachedWorkOrderTypes()
-  const workOrderStatuses = getCachedWorkOrderStatuses()
 
   response.render('workOrders/edit', {
     headTitle: i18next.t('workOrders.newWorkOrder', {
